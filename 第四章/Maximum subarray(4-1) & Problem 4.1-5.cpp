881befebb4e81227037af31f27980e5574b23327
SToPire <46126592+SToPire@users.noreply.github.com>
2019-03-12 13:23:16 +0800

    Update Maximum subarray(4-1) & Problem 4.1-5.cpp

--- 第四章/Maximum subarray(4-1) & Problem 4.1-5.cpp
+++ 第四章/Maximum subarray(4-1) & Problem 4.1-5.cpp
@@ -2,12 +2,12 @@
 #include<vector>
 #include<tuple>
 using namespace std;
-auto find_max_cross_subarry(vector<int>& v, int begin, int mid, int end)-> tuple<int,int,int>
+auto find_max_cross_subarry(vector<int>& v, int begin, int mid, int end)-> tuple<int, int, int>
 {
 	int lmax = v[mid], rmax = v[mid + 1];
 	int sum = v[mid];
 	int lindex = mid, rindex = mid + 1;
-	for (int i = mid - 1 ; i >= begin; --i)
+	for (int i = mid - 1; i >= begin; --i)
 	{
 		sum += v[i];
 		if (sum > lmax) {
@@ -26,7 +26,7 @@ auto find_max_cross_subarry(vector<int>& v, int begin, int mid, int end)-> tuple
 	}
 	return { lindex,rindex,lmax + rmax };
 }
-auto find_max_subarray(vector<int>& v,int begin,int end) ->tuple<int,int,int>
+auto find_max_subarray(vector<int>& v, int begin, int end) ->tuple<int, int, int>
 {
 	if (begin == end) return{ begin,end,v[begin] };
 	int mid = (begin + end) / 2;
@@ -37,10 +37,10 @@ auto find_max_subarray(vector<int>& v,int begin,int end) ->tuple<int,int,int>
 	else if (get<2>(rres) >= get<2>(lres) and get<2>(rres) >= get<2>(crossres)) return rres;
 	else return crossres;
 }
-auto linearComplexity(vector<int>& v, int begin, int end) ->tuple<int, int, int> //思考题4.1-5
+auto linearComplexity(vector<int>& v, int begin, int end) ->tuple<int, int, int>
 {
 	int sum = 0, max = 0, lindex = 0, rindex = 0, temp = 0;
-	for (int i = begin;i != end; ++i)
+	for (int i = begin; i != end; ++i)
 	{
 		sum += v[i];
 		if (sum > max) {         //仅当出现和更大的序列时，更新三个输出变量
@@ -55,13 +55,36 @@ auto linearComplexity(vector<int>& v, int begin, int end) ->tuple<int, int, int>
 	}
 	return { lindex,rindex,max };
 }
+auto dp(vector<int>&v, int begin, int end)->tuple<int, int, int> //练习4.1-5,动态规划，遍历两遍数组，复杂度为O(n) 
+{
+	vector<tuple<int,int,int>> s(end - begin + 1); //s[j]表示以v[j]结尾的最大子数组
+	s[begin] = { begin,begin,v[begin] };
+	for (int i = begin + 1; i <= end; ++i)
+	{
+		if (get<2>(s[i-1]) <= 0)   //如果以v[i-1]结尾的最大子数组之和为负数，那么以v[i]结尾的最大子数组就是v[i]本身
+			s[i] = { i,i,v[i] };
+		else
+			s[i] = { get<0>(s[i - 1]),i,get<2>(s[i - 1]) + v[i] };//否则就为v[i-1]的最大子数组拼接上v[i]
+	}
+	int max = get<2>(s[begin]);
+	int maxindex = 0;
+	for (int i = begin + 1; i <= end; ++i) //找出最大的s[i]
+	{
+		if (get<2>(s[i]) > max) {
+			max = get<2>(s[i]);
+			maxindex = i;
+		}
+	}
+	return s[maxindex];
+}
 int main()
 {
 	vector<int> vi{ 13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7 };
-	auto res = find_max_subarray(vi, 0, vi.size() - 1);
+	//auto res = find_max_subarray(vi, 0, vi.size() - 1);
 	//auto res = linearComplexity(vi, 0, vi.size() - 1);
+	auto res = dp(vi, 0, vi.size() - 1);
 	cout << "Maximum subarray is:" << endl;
-	for (int i = get<0>(res); i <= get<1>(res); ++i) cout << vi[i]<<' ';
+	for (int i = get<0>(res); i <= get<1>(res); ++i) cout << vi[i] << ' ';
 	cout << endl << "with max sum of " << get<2>(res);
 	system("pause");
 }
